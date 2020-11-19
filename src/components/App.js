e868df6ab2b388fb76c8dbc30f089311f54c9fbe
WahidX <wahidx93@gmail.com>
2020-11-19 21:33:36 +0530

    store added

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className="App">Hi SomeX</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { configureStore } from './store';
+
+const store = configureStore();
+console.log(store.getState());
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
