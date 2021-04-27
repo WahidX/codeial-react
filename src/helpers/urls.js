703a29487457c177814c21a507b0c87b8e873e84
WahidX <wahidx93@gmail.com>
2021-04-27 14:20:59 +0530

    api port changed

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,6 +1,6 @@
 // export const SERVER_ROOT = 'https://codeialx.herokuapp.com';
 // export const SERVER_ROOT = 'http://localhost:8000';
-export const SERVER_ROOT = 'http://13.233.196.0:8000';
+export const SERVER_ROOT = 'http://13.233.196.0';
 const CHAT_SERVER = 'http://13.233.196.0:5000';
 
 const API_ROOT = `${SERVER_ROOT}/api/v1`;
